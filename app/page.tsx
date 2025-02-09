83d8aa1988cb0b5cdb97fd2c1bb070a282e65457
Nabil Belfki <nabilbelfki@gmail.com>
2025-02-09 12:02:52 -0500

    Preparing to receive changes to project for calendar popup and Projects page from work laptop

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,7 @@ export default function Home() {
         imageAlt="Nabil Belfki"
         biography="There is nothing that I can’t do or accomplish. I’m a great asset to a team and company. I’ve transformed countless clients in my career by helping them automate processes and solve their problems. I’m code agnostic haven write projects in many different stacks. I have knowledge in many different sectors from the flight industry to banking and finance to e-commerce."
       />
-      {/* <Popup 
-        title="Schedule a Meeting"
-        body={}
-        actions={<Button text="Book" onClick={ bookMeeting } />}
-      /> */}
+      {/* < */}
       <div className="experience-and-skills">
         <Timeline />
         <div className="skills-display">
