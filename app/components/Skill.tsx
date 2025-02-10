cdc05e858a440e6669bac788536d82e5672c727d
Nabil Belfki <nbelfki@colesolutions.net>
2025-02-09 23:48:05 -0500

    Fixed Skills Grid System

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -3,30 +3,37 @@ import Image from "next/image";
 import styles from "./Skill.module.css";
 
 interface SkillProps {
+  gridArea: string;
   className: string;
   backgroundColor: string;
   height: number;
   logoPath: string;
   description: string;
+  onClick: () => void;
+  showDescription: boolean;
 }
 
 const Skill: React.FC<SkillProps> = ({
+  gridArea,
   className,
   backgroundColor,
   logoPath,
   height,
   description,
+  onClick,
+  showDescription,
 }) => {
   return (
-    <div style={{ backgroundColor }} className={`${styles.skill} ${className}`}>
-      <div className={styles.description}>{description}</div>
+    <div
+      style={{ backgroundColor, gridArea }}
+      className={className}
+      onClick={onClick}
+    >
+      {showDescription && (
+        <div className={styles.description}>{description}</div>
+      )}
       <div className={styles.logo}>
-        <Image
-          src={logoPath}
-          height={height}
-          width="200"
-          alt="Logo"
-        />
+        <Image src={logoPath} height={height} width="200" alt="Logo" />
       </div>
     </div>
   );
