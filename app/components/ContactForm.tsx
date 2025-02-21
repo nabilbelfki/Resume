37645f80db0195493ab1afe28b1e753b671b8f2a
Nabil Belfki <nbelfki@colesolutions.net>
2025-02-20 19:34:02 -0500

    Adding scrubber and vibration to Skills Component

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Button from "./Button";
 import styles from "./ContactForm.module.css";
@@ -11,41 +11,61 @@ const Contact: React.FC<unknown> = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-      setFirstName(e.target.value);
-    const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-      setLastName(e.target.value);
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-      setEmail(e.target.value);
-    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-      setMessage(e.target.value);
+  const [firstNameStyles, setFirstNameStyles] = useState({ border: "none" });
+  const [lastNameStyles, setLastNameStyles] = useState({ border: "none" });
+  const [emailStyles, setEmailStyles] = useState({ border: "none" });
+  const [messageStyles, setMessageStyles] = useState({ border: "none" });
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+    setFirstNameStyles({ border: "none" });
+  };
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+    setLastNameStyles({ border: "none" });
+  };
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailStyles({ border: "none" });
+  };
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+    setMessageStyles({ border: "none" });
+  };
 
   const sendEmail = () => {
-    const templateParams = {
-      firstName,
-      lastName,
-      email,
-      message
-    };
+    if (firstName != "" && lastName != "" && email != "" && message != "") {
+      const templateParams = {
+        firstName,
+        lastName,
+        email,
+        message,
+      };
 
-    fetch("/api/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(templateParams),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          console.log("Email sent successfully");
-        } else {
-          console.log("Failed to send email");
-        }
+      fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(templateParams),
       })
-      .catch((error) => {
-        console.error("Error sending email:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            console.log("Email sent successfully");
+          } else {
+            console.log("Failed to send email");
+          }
+        })
+        .catch((error) => {
+          console.error("Error sending email:", error);
+        });
+    } else {
+      setFirstNameStyles({ border: "solid 1px red" });
+      setLastNameStyles({ border: "solid 1px red" });
+      setEmailStyles({ border: "solid 1px red" });
+      setMessageStyles({ border: "solid 1px red" });
+    }
   };
 
   return (
@@ -58,24 +78,41 @@ const Contact: React.FC<unknown> = () => {
           className={styles["first-name"]}
           type="text"
           placeholder="First Name..."
+          style={firstNameStyles}
           onChange={handleFirstNameChange}
         />
         <input
           className={styles["last-name"]}
           type="text"
           placeholder="Last Name..."
+          style={lastNameStyles}
           onChange={handleLastNameChange}
         />
         <input
           className={styles["email"]}
           type="text"
           placeholder="Email Address..."
+          style={emailStyles}
           onChange={handleEmailChange}
         />
       </div>
       <div className={styles["message"]}>
-        <textarea placeholder="Then reach out to me and let’s discuss it..." onChange={handleMessageChange}></textarea>
-        <Button text="Send" onClick={sendEmail} style={{ bottom: 10, right: 5, position: "absolute", fontWeight: 600, fontSize: 16}}/>
+        <textarea
+          placeholder="Then reach out to me and let’s discuss it..."
+          style={messageStyles}
+          onChange={handleMessageChange}
+        ></textarea>
+        <Button
+          text="Send"
+          onClick={sendEmail}
+          style={{
+            bottom: 10,
+            right: 5,
+            position: "absolute",
+            fontWeight: 600,
+            fontSize: 16,
+          }}
+        />
       </div>
     </div>
   );
