e76da4f9f601bc091d4ffff8faf36d91c6fdc6c2
Nabil Belfki <nbelfki@colesolutions.net>
2025-02-26 23:34:48 -0500

    Timeline.tsx

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Experience from "./Experience";
 import styles from "./Timeline.module.css";
-import { Experiences } from './types';
+import { Experiences } from "./types";
 
 interface TimelineProps {
   experiences: Experiences[];
@@ -10,28 +10,29 @@ interface TimelineProps {
 
 const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
   const [positions, setPositions] = useState<{ [key: string]: number }>({});
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null); // Initialize state for hovered index
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
   const timelineRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const calculatePositions = () => {
     if (timelineRef.current) {
       const months = timelineRef.current.querySelectorAll(
         `.${styles.indicator}`
       );
       const newPositions: { [key: string]: number } = {};
       const timelineRect = timelineRef.current.getBoundingClientRect();
-  
+
       months.forEach((month) => {
         const parentElement = month.parentElement;
         if (parentElement) {
           const parentClass = parentElement.className.split(" ");
           const monthYear = parentClass ? parentClass[1] : null;
-  
+
           if (monthYear) {
-            const left = parentElement.getBoundingClientRect().left - timelineRect.left; // Adjust based on timeline's left position
-            console.log(`Month: ${monthYear}, Left: ${left}`); // Debug log
-  
-            // Adjust for January being part of the year div
+            const left =
+              parentElement.getBoundingClientRect().left - timelineRect.left;
+            console.log(`Month: ${monthYear}, Left: ${left}`);
+
             const isJanuary = parentClass.includes(styles.year);
             if (isJanuary) {
               const yearClass = parentClass.find((cls) => cls.startsWith("20"));
@@ -43,11 +44,21 @@ const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
           }
         }
       });
-  
-      console.log("New Positions:", newPositions); // Debug log
+
+      console.log("New Positions:", newPositions);
       setPositions(newPositions);
+      setLoading(false);
     }
-  }, []);  
+  };
+
+  useEffect(() => {
+    calculatePositions();
+    window.addEventListener("resize", calculatePositions);
+
+    return () => {
+      window.removeEventListener("resize", calculatePositions);
+    };
+  }, []);
 
   const generateMonths = () => {
     const months = [
@@ -67,13 +78,12 @@ const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
     const elements = [];
-  
+
     for (let year = 2017; year <= currentYear; year++) {
       for (let month = 0; month < 12; month++) {
         if (year === currentYear && month > currentMonth) break;
         const monthYearClass = `${year}-${months[month].toLowerCase()}`;
         if (month === 0) {
-          // Create a single div for the year with "January" as part of that div
           elements.push(
             <div key={year} className={`${styles.year} ${monthYearClass}`}>
               <div className={styles.indicator}></div>
@@ -84,7 +94,9 @@ const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
           elements.push(
             <div
               key={monthYearClass}
-              className={`${styles[months[month].toLowerCase()]} ${monthYearClass}`}
+              className={`${
+                styles[months[month].toLowerCase()]
+              } ${monthYearClass}`}
             >
               <div className={styles.indicator}></div>
             </div>
@@ -92,23 +104,24 @@ const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
         }
       }
     }
-  
+
     return elements;
-  };  
+  };
 
   return (
     <div className={styles.timeline} ref={timelineRef}>
-      {experiences.map((experience, index) => (
-        <Experience
-          key={index}
-          index={index}
-          experience={experience}
-          positions={positions}
-          hoveredIndex={hoveredIndex}
-          setHoveredIndex={setHoveredIndex}
-        />
-      ))}
       {generateMonths()}
+      {!loading &&
+        experiences.map((experience, index) => (
+          <Experience
+            key={index}
+            index={index}
+            experience={experience}
+            positions={positions}
+            hoveredIndex={hoveredIndex}
+            setHoveredIndex={setHoveredIndex}
+          />
+        ))}
     </div>
   );
 };
