050a58019cc5847fa222f8d015cb7a5b3b6d5daf
Nabil Belfki <nbelfki@colesolutions.net>
2025-02-28 22:28:50 -0500

    Fixing issue with Timezones at the end of the month where it was breaking the timeline

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import styles from "./Experience.module.css";
-import { Experiences } from './types';
+import { Experiences } from "./types";
 
 interface ExperienceProps {
   experience: Experiences;
@@ -32,67 +32,185 @@ const Experience: React.FC<ExperienceProps> = ({
     "november",
     "december",
   ];
+
+  const currentYear = new Date().getFullYear();
+  const currentMonth = months[new Date().getMonth()];
+  console.log(currentMonth);
+  let endYearMonth = `${currentYear}-${currentMonth}`;
+
   const startDate = experience.period.start;
-  const endDate = experience.period.end || new Date().toISOString().split('T')[0]; // Default to today if endDate is missing
+
+  if (experience.period.end) {
+    const endDate = experience.period.end; // Default to today if endDate is missing
+    const [endYear, endMonth] = endDate.slice(0, 7).split("-"); // Extracts year and month as YYYY-MM
+    endYearMonth = endYear + "-" + months[parseInt(endMonth) - 1];
+  }
+
   const [startYear, startMonth] = startDate.slice(0, 7).split("-"); // Extracts year and month as YYYY-MM
-  const [endYear, endMonth] = endDate.slice(0, 7).split("-"); // Extracts year and month as YYYY-MM 
-  const startYearMonth = startYear + "-" + months[(parseInt(startMonth) - 1)];
-  const endYearMonth = endYear + "-" + months[(parseInt(endMonth) - 1)];
+  const startYearMonth = startYear + "-" + months[parseInt(startMonth) - 1];
+
   const startX = positions[startYearMonth];
   const endX = positions[endYearMonth];
   const duration = Math.abs(startX - endX);
-  console.log(`Experience - Start: ${startX}, End: ${endX}, Duration: ${duration}`); // Debug log
+  console.log(
+    `Experience - Start: ${startX}, End: ${endX}, Duration: ${duration}`
+  ); // Debug log
   const top = experience.level == 1 ? 393 : 353;
   const zIndex = hoveredIndex == index ? 10 : experience.zIndex;
-  const informationTop = hoveredIndex == index ? (experience.name == "Cole Solutions LLC" ? -360 : -320) : (experience.name == "Cole Solutions LLC" ? -240 : -200);
-  const translateX = experience.name == "Cole Solutions LLC" ? "translateX(calc(-50% - 70px))" : (experience.name == "New Jersey Institute of Technology" ? "translateX(calc(-50% + 100px))" : "translateX(-50%)");
-  const animationClass = experience.name == "Cole Solutions LLC" ? "floating-cole" : (experience.name == "New Jersey Institute of Technology" ? "floating-njit" : "floating");
+  const informationTop =
+    hoveredIndex == index
+      ? experience.name == "Cole Solutions LLC"
+        ? -360
+        : -320
+      : experience.name == "Cole Solutions LLC"
+      ? -240
+      : -200;
+  const translateX =
+    experience.name == "Cole Solutions LLC"
+      ? "translateX(calc(-50% - 70px))"
+      : experience.name == "New Jersey Institute of Technology"
+      ? "translateX(calc(-50% + 100px))"
+      : "translateX(-50%)";
+  const animationClass =
+    experience.name == "Cole Solutions LLC"
+      ? "floating-cole"
+      : experience.name == "New Jersey Institute of Technology"
+      ? "floating-njit"
+      : "floating";
   return (
     <div
       className={styles.experience}
       style={{
         top: `${top}px`,
         left: `${endX}px`,
-        width: `${experience.level == 1 ? (experience.name == "American College of Thessaloniki" ? duration + 8 : duration + 12) : duration - 75}px`,
+        width: `${
+          experience.level == 1
+            ? experience.name == "American College of Thessaloniki"
+              ? duration + 8
+              : duration + 12
+            : duration - 75
+        }px`,
       }}
     >
-      <div className={index != hoveredIndex ? `${styles.information} ${styles[animationClass]}` : styles.information } style={{backgroundColor: experience.color.background, zIndex: zIndex, top: informationTop, transform: translateX}}
-      onMouseEnter={() => setHoveredIndex(index)} // Set state to index on hover
-      onMouseLeave={() => setHoveredIndex(null)} // Set state to null when hover ends
+      <div
+        className={
+          index != hoveredIndex
+            ? `${styles.information} ${styles[animationClass]}`
+            : styles.information
+        }
+        style={{
+          backgroundColor: experience.color.background,
+          zIndex: zIndex,
+          top: informationTop,
+          transform: translateX,
+        }}
+        onMouseEnter={() => setHoveredIndex(index)} // Set state to index on hover
+        onMouseLeave={() => setHoveredIndex(null)} // Set state to null when hover ends
       >
         <div className={styles.header}>
           <div className={styles.logo}>
-            { index == hoveredIndex && (<div className={styles.opened}>
-              <Image src={"/images/" + experience.logo.opened.name} alt={`${experience.name} Logo`} width={experience.logo.opened.width} height={experience.logo.opened.height}/>
-            </div>)}
-            { index != hoveredIndex && (<div className={styles.closed}>
-              <Image src={"/images/" + experience.logo.closed.name} alt={`${experience.name} Logo`} width={experience.logo.closed.width} height={experience.logo.closed.height} />
-            </div>)}
+            {index == hoveredIndex && (
+              <div className={styles.opened}>
+                <Image
+                  src={"/images/" + experience.logo.opened.name}
+                  alt={`${experience.name} Logo`}
+                  width={experience.logo.opened.width}
+                  height={experience.logo.opened.height}
+                />
+              </div>
+            )}
+            {index != hoveredIndex && (
+              <div className={styles.closed}>
+                <Image
+                  src={"/images/" + experience.logo.closed.name}
+                  alt={`${experience.name} Logo`}
+                  width={experience.logo.closed.width}
+                  height={experience.logo.closed.height}
+                />
+              </div>
+            )}
           </div>
-          { index == hoveredIndex && (<div className={styles["name-location-and-date"]}>
-            <div className={styles.name} style={{color: experience.color.name }}>{experience.name}</div>
-            <div className={styles["location-and-date"]}>
-              <div className={styles.location} style={{color: experience.color.location }}>{experience.location}</div>
-              <div className={styles.date} style={{color: experience.color.date }}>{experience.period.title}</div>
+          {index == hoveredIndex && (
+            <div className={styles["name-location-and-date"]}>
+              <div
+                className={styles.name}
+                style={{ color: experience.color.name }}
+              >
+                {experience.name}
+              </div>
+              <div className={styles["location-and-date"]}>
+                <div
+                  className={styles.location}
+                  style={{ color: experience.color.location }}
+                >
+                  {experience.location}
+                </div>
+                <div
+                  className={styles.date}
+                  style={{ color: experience.color.date }}
+                >
+                  {experience.period.title}
+                </div>
+              </div>
             </div>
-          </div>)}
+          )}
         </div>
-        { index == hoveredIndex && (<div className={styles.details} style={{backgroundColor: experience.color.details }}>
-          <div className={styles.subheader}>
-            <div className={styles["title-and-subtitle"]}>
-              <span className={styles.title} style={{color: experience.color.title }}>{experience.title}</span>
-              {experience.subtitle && (
-                <span className={styles.subtitle} style={{color: experience.color.subtitle }}>{" " + experience.subtitle}</span>
-              )}
+        {index == hoveredIndex && (
+          <div
+            className={styles.details}
+            style={{ backgroundColor: experience.color.details }}
+          >
+            <div className={styles.subheader}>
+              <div className={styles["title-and-subtitle"]}>
+                <span
+                  className={styles.title}
+                  style={{ color: experience.color.title }}
+                >
+                  {experience.title}
+                </span>
+                {experience.subtitle && (
+                  <span
+                    className={styles.subtitle}
+                    style={{ color: experience.color.subtitle }}
+                  >
+                    {" " + experience.subtitle}
+                  </span>
+                )}
+              </div>
+              <div
+                className={styles.type}
+                style={{ color: experience.color.type }}
+              >
+                {experience.type}
+              </div>
             </div>
-            <div className={styles.type} style={{color: experience.color.type }}>{experience.type}</div>
+            <div
+              className={styles.description}
+              style={{
+                backgroundColor: experience.color.description.background,
+                color: experience.color.description.text,
+              }}
+            >
+              {experience.description}
+            </div>
           </div>
-          <div className={styles.description} style={{backgroundColor: experience.color.description.background, color: experience.color.description.text }}>{experience.description}</div>
-        </div>)}
+        )}
       </div>
-      <div className={styles.line} style={{backgroundColor: experience.color.line}}></div>
+      <div
+        className={styles.line}
+        style={{ backgroundColor: experience.color.line }}
+      ></div>
       {experience.level == 2 && (
-        <div className={styles.line} style={{backgroundColor: experience.color.line, transform: 'rotate(30deg)', width: 82, marginLeft: duration - 82, marginTop: 13}}></div>
+        <div
+          className={styles.line}
+          style={{
+            backgroundColor: experience.color.line,
+            transform: "rotate(30deg)",
+            width: 82,
+            marginLeft: duration - 82,
+            marginTop: 13,
+          }}
+        ></div>
       )}
     </div>
   );
