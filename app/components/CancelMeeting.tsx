02b25a50f2857b4ab16d403019d5dc28b7dd5068
Nabil Belfki <nabilbelfki@gmail.com>
2025-06-20 16:49:42 -0400

    Fixing Skills Bug and Footer Link Alignment on Mobile

--- app/components/CancelMeeting.tsx
+++ app/components/CancelMeeting.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Button from "../components/Button";
 import styles from "./CancelMeeting.module.css";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 interface CancelMeetingProps {
   firstName: string;
   lastName: string;
